ff19107add3666b2ab5f6f3dc0f61deb976b046f
RogerK0bs <107825336+RogerK0bs@users.noreply.github.com>
2023-02-01 03:21:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,14 +41,6 @@ public:
 		cout << " \n\n\t   " << numerator << "\n\t " << integer; LINE_D;
 		cout << "\n \t   " << denominator << endl;
 	}
-	Fraction& operator()(int integer, int numerator, int denominator)
-	{
-		Set_integer(integer);
-		Set_numerator(numerator);
-		Set_denominator(denominator);
-		if (denominator == 0)this->denominator = 1;
-		return *this;
-	}
 	Fraction& to_proper()
 	{
 		if (integer > 0)
@@ -56,11 +48,11 @@ public:
 			int a = integer * denominator + numerator;
 			integer = 0;
 			numerator = a;
-			cout << "\nЦелая часть интегрируется в числитель = ";
-			Print();
+			//cout << "\nЦелая часть интегрируется в числитель = ";
+		//	Print();
 			return *this;
 		}
-		else cout << "to_proper() - целая часть отсутствует!";
+		//else cout << "to_proper() - целая часть отсутствует!";
 	}
 	Fraction& to_improper()
 	{
@@ -71,8 +63,8 @@ public:
 				this->numerator -= denominator;
 				this->integer++;
 			} while (numerator >= denominator);
-			cout << "\nВыделение целой части из числителя = ";
-			Print();
+			//	cout << "\nВыделение целой части из числителя = ";
+		//	Print();
 			return *this;
 		}
 		else
@@ -81,17 +73,74 @@ public:
 			{
 				this->integer++;
 				this->numerator -= denominator;
-				cout << "\nВыделение целой части из числителя = ";
-				Print();
+				//	cout << "\nВыделение целой части из числителя = ";
+			//	Print();
 				return *this;
 			}
-			else cout << "\nto_improper() - числитель меньше знаменателя!";
+			//	else cout << "\nto_improper() - числитель меньше знаменателя!";
 
 		}
 	}
+	Fraction& swap()
+	{
+		if (integer > 0) { numerator = integer * denominator + numerator; integer = 0; }
+		int buffer = numerator;
+		this->numerator = denominator;
+		this->denominator = buffer;
+		return *this;
+	}
 	// Операторы 
-
-
+	Fraction& operator()(int integer, int numerator, int denominator)
+	{
+		Set_integer(integer);
+		Set_numerator(numerator);
+		Set_denominator(denominator);
+		if (denominator == 0)this->denominator = 1;
+		return *this;
+	}
+	Fraction operator=(Fraction& Drob)
+	{
+		this->numerator = Drob.numerator;
+		this->denominator = Drob.denominator;
+		if (Drob.Get_integer() > 0)this->integer = Drob.integer;
+		return *this;
+	}
+	Fraction operator+=(Fraction& Drob)
+	{
+		if (Drob.Get_integer() > 0)Drob.to_proper();
+		if (integer > 0) { numerator = integer * denominator + numerator; integer = 0; }
+		int buffer = numerator;
+		this->numerator = ((numerator * Drob.Get_denominator())+(Drob.Get_numerator()*denominator));
+		this->denominator = ((denominator * Drob.Get_numerator())+(Drob.Get_denominator()*buffer));
+		return *this;
+	}
+	Fraction operator-=(Fraction& Drob)
+	{
+		if (Drob.Get_integer()>0)Drob.to_proper();
+		if (integer > 0) { numerator = integer * denominator + numerator; integer = 0; }
+		int buffer = numerator;
+		this->numerator = ((numerator * Drob.Get_denominator()) - (Drob.Get_numerator() * denominator));
+		this->denominator = ((denominator * Drob.Get_numerator()) - (Drob.Get_denominator() * buffer));
+		return *this;
+	}
+	Fraction operator*=(Fraction& Drob)
+	{
+		if (Drob.Get_integer() > 0)Drob.to_proper();
+		if (integer > 0) {numerator = integer * denominator + numerator; integer = 0;}
+		int buffer = numerator;
+		this->numerator = numerator * Drob.Get_numerator();
+		this->denominator=denominator * Drob.Get_denominator();
+		return *this;
+	}
+	Fraction operator/=(Fraction& Drob)
+	{
+		if (Drob.Get_integer() > 0)Drob.to_proper();
+		if (integer > 0) { numerator = integer * denominator + numerator; integer = 0; }
+		int buffer = numerator; Drob.swap();
+		this->numerator *= Drob.Get_numerator();
+		this->denominator *= Drob.Get_denominator();
+		return *this;
+	}
 };
 
 std::istream& operator>>(std::istream& is, Fraction& obj)
@@ -104,12 +153,12 @@ std::istream& operator>>(std::istream& is, Fraction& obj)
 	system("cls");
 	return is;
 }
-Fraction operator+( Fraction& Drob1,  Fraction& Drob2)
+Fraction operator+(Fraction& Drob1, Fraction& Drob2)
 {
 	Fraction result;
 	if (Drob1.Get_denominator() == Drob2.Get_denominator())
 	{
-		result.Set_integer(Drob1.Get_integer() + Drob1.Get_integer());
+		result.Set_integer(Drob1.Get_integer() + Drob2.Get_integer());
 		result.Set_numerator(Drob1.Get_numerator() + Drob1.Get_numerator());
 		return result;
 	}
@@ -123,6 +172,40 @@ Fraction operator+( Fraction& Drob1,  Fraction& Drob2)
 		return result;
 	}
 
+}
+Fraction operator-(Fraction& Drob1, Fraction& Drob2)
+{
+	Fraction result;
+	if (Drob1.Get_denominator() == Drob2.Get_denominator())
+	{
+		result.Set_integer(Drob1.Get_integer() - Drob2.Get_integer());
+		result.Set_numerator(Drob1.Get_numerator() - Drob1.Get_numerator());
+		return result;
+	}
+	else
+	{
+		Drob1.to_proper();
+		Drob2.to_proper();
+		result.Set_numerator(((Drob1.Get_numerator() * Drob2.Get_denominator()) - (Drob2.Get_numerator() * Drob1.Get_denominator())));
+		result.Set_denominator(((Drob1.Get_denominator() * Drob2.Get_numerator()) - (Drob2.Get_denominator() * Drob1.Get_numerator())));
+		result.Set_integer(0);
+		return result;
+	}
+}
+Fraction operator* (Fraction& Drob1, Fraction& Drob2)
+{
+	Fraction result;
+	Drob1.to_proper();
+	Drob2.to_proper();
+	result.Set_numerator(Drob1.Get_numerator() * Drob2.Get_numerator());
+	result.Set_denominator(Drob1.Get_denominator() * Drob2.Get_denominator());
+	result.to_improper();
+	return result;
+}
+Fraction operator/(Fraction& Drob1, Fraction& Drob2)
+{
+	Drob2.swap();
+	return Drob1*Drob2;
 }
 
 void main()
@@ -143,9 +226,10 @@ void main()
 	cin >> IND2;
 	cout << "Дробь №1 = "; IND1.Print();
 	cout << "Дробь №2 = "; IND2.Print();
-	IND1 = IND1 + IND2;
+	IND1 += IND2;
 	IND1.to_improper();
 	IND1.Print();
+
 
 	_getch();
 }
